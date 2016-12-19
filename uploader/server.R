4ca1ca433547427ed3f9c3dacd69592e16ed6bb6
LukeLinEx <lukelinex@gmail.com>
2016-12-18 21:14:49 -0500

    uploader

---
+++ uploader/server.R
@@ -0,0 +1,28 @@
+library(shiny)
+
+function(input, output) {
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, it will be a data frame with 'name',
+    # 'size', 'type', and 'datapath' columns. The 'datapath'
+    # column will contain the local filenames where the data can
+    # be found.
+    
+    inFile <- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    fname = input$file1$name
+    path = input$file1$datapath
+    print(path)
+    #fname = paste(path)
+    file.copy(from=path, to=paste0('~/Desktop/uploader/', fname))
+  
+    
+    
+    # read.csv(inFile$datapath, header=input$header, sep=input$sep, 
+    #          quote=input$quote)
+  })
+}
